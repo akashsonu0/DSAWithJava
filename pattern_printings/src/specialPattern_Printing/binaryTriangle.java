2cb982a489ee890718bd5e2fd266907c6d837386
akashsonu0 <akashsonu7744@gmail.com>
2025-11-25 10:13:32 +0530

    binaryTriangle

---
+++ pattern_printings/src/specialPattern_Printing/binaryTriangle.java
@@ -0,0 +1,35 @@
+package specialPattern_Printing;
+
+import java.util.Scanner;
+
+public class binaryTriangle {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n : ");
+		int n = sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+//				if(i%2==1) { // odd case  
+//					if(j%2==1) System.out.print(1 + " ");
+//					else System.out.print(0 + " ");
+//				}
+//				else { // i%2==0 , even case
+//					if(j%2==0) System.out.print(1 + " ");
+//					else System.out.print(0 + " ");
+//				}
+//			}
+//			System.out.println();	
+//		}
+//		
+		// 2nd Method
+		if((i+j)%2==0) System.out.print(1 + " ");
+			else System.out.print(0 + " ");
+		}
+		
+	System.out.println();
+		}
+	}
+}
+
+
