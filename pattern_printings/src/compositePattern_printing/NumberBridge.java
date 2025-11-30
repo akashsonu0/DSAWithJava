7701234cc215242225cb24f4c4da708be41a7d4a
akashsonu0 <akashsonu7744@gmail.com>
2025-11-30 10:10:14 +0530

    numberBridge

--- pattern_printings/src/compositePattern_printing/NumberBridge.java
+++ pattern_printings/src/compositePattern_printing/NumberBridge.java
@@ -2,30 +2,35 @@ package compositePattern_printing;
 
 import java.util.Scanner;
 
-public class starBridge {
+public class NumberBridge {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter n : ");
 		int n = sc.nextInt();
 		for(int i=1;i<=2*n-1;i++) {
-			System.out.print("* ");
+			System.out.print(i + " ");
 		}
 		System.out.println();
 		n--;
-		for(int i=1;i<=n;i++) {
-			for(int j=1;j<=n+1-i;j++) { // print stars
-				System.out.print("* ");
+		int nsp = 1;
+		for(int i=1;i<=n;i++) { //rows
+			int a = 1;
+			for(int j=1;j<=n+1-i;j++) { // print numbers
+				System.out.print(a++ + " ");
 			}
-			for(int j=1;j<=2*i-1;j++) { // print spaces
+			for(int j=1;j<=nsp;j++) { // print spaces
 				System.out.print(" " + " ");
+				a++;
 			}
-			for(int j=1;j<=n+1-i;j++) { // print stars
-				System.out.print("* ");
+			nsp +=2;
+			for(int j=5;j<=n+5-i;j++) { // print numbers
+				System.out.print(a++ + " ");
 			}
 			System.out.println();
 			
 		}
+
 	}
 
 }
