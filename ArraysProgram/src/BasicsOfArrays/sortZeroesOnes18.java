8576eba56fb137d216b5ab7053d6f8c6247606c2
akashsonu0 <akashsonu7744@gmail.com>
2026-01-22 20:04:30 +0530

    sortZeroesOne

--- ArraysProgram/src/BasicsOfArrays/sortZeroesOnes18.java
+++ ArraysProgram/src/BasicsOfArrays/sortZeroesOnes18.java
@@ -3,7 +3,7 @@ package BasicsOfArrays;
 public class sortZeroesOnes18 {
 
 	public static void main(String[] args) {
-		int[] arr = {1,1,0,0,1,1,0,0,0,1,0};
+		int[] arr = {0,0,0,1,1,1};
 		int n = arr.length;
 		//1st method
 //		int noOfZeroes = 0;
@@ -26,7 +26,7 @@ public class sortZeroesOnes18 {
 		while(i<j) {
 			if(arr[i]==0) i++;
 			if(arr[j]==1) j--;
-			//if(i>j) break;
+			//if(i<) break;
 			if(i<j && arr[i]==1 && arr[j]==0) {
 				arr[i]=0;
 				arr[j]=1;
