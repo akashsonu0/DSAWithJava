e43c871d643c01fd2ed2b2bb34c7065c791923c3
akashsonu0 <akashsonu7744@gmail.com>
2026-01-28 17:23:02 +0530

    outputUsingNestedLoops

--- ArraysProgram/src/TwoDArrays/outputUsingNestedLoops02.java
+++ ArraysProgram/src/TwoDArrays/outputUsingNestedLoops02.java
@@ -2,7 +2,7 @@ package TwoDArrays;
 
 import java.util.Scanner;
 
-public class outputUsingNestedLoops {
+public class outputUsingNestedLoops02 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
