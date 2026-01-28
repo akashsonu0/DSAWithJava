0a600ef613fc3ec5db5fdf9efad8088e2cbc3b24
akashsonu0 <akashsonu7744@gmail.com>
2026-01-28 17:42:57 +0530

    maxInArrays

---
+++ ArraysProgram/src/TwoDArrays/maxInArray05.java
@@ -0,0 +1,19 @@
+package TwoDArrays;
+
+public class maxInArray05 {
+
+	public static void main(String[] args) {
+		int[][] arr = {{1,5,6,},{2,-9,3},{5,10,3}};
+		int mx = Integer.MIN_VALUE;
+		int m = arr.length;
+		int n = arr[0].length;
+		for(int i=0;i<m;i++) {
+			for(int j=0;j<n;j++) {
+				mx = Math.max(mx,arr[i][j]);
+			}
+		}
+		System.out.println(mx);
+		
+	}
+
+}
