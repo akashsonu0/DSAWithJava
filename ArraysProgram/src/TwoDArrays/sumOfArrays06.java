b188f1dc237823ce72aa3688f6efab001d504d03
akashsonu0 <akashsonu7744@gmail.com>
2026-01-28 21:46:26 +0530

    sumOfArrays

--- ArraysProgram/src/TwoDArrays/sumOfArrays06.java
+++ ArraysProgram/src/TwoDArrays/sumOfArrays06.java
@@ -4,7 +4,6 @@ public class sumOfArrays06 {
 
 	public static void main(String[] args) {
 		int[][] arr = {{1,5,6,},{2,-9,30},{5,10,3}};
-		int mx = Integer.MIN_VALUE;
 		int m = arr.length;
 		int n = arr[0].length;
 		int sum =0;
