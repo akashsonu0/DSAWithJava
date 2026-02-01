fe7fcee4a551fe2083b660e54e2df9adc23def46
akashsonu0 <akashsonu7744@gmail.com>
2026-02-01 20:10:20 +0530

    arrayList2D

---
+++ ArraysProgram/src/TwoDArrays/ArrayList2D13.java
@@ -0,0 +1,31 @@
+package TwoDArrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList2D13 {
+
+	public static void main(String[] args) {
+		List<Integer> a = new ArrayList<>();
+		a.add(10); a.add(20); a.add(30);
+		List<Integer> b = new ArrayList<>();
+		b.add(40); b.add(50);
+		List<Integer> c= new ArrayList<>();
+		List<Integer> d = new ArrayList<>();
+		d.add(60); 
+		List<List<Integer>> l =new ArrayList<>();
+		l.add(a); l.add(b); l.add(c); l.add(d);
+		
+//		for(int i=0;i<l.size();i++) {
+//			System.out.println(l.get(i));
+//		}
+		for(int i=0;i<l.size();i++) {
+			for(int j=0;j<l.get(i).size();j++) {
+				System.out.print(l.get(i).get(j)+" ");
+			}
+			System.out.println();
+		}
+		//System.out.println(l.get(1).get(0));
+	}
+
+}
