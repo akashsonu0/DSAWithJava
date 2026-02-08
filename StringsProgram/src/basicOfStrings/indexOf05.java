6b7461b74c48f2eda75c15054f92d2f9dcda1347
akashsonu0 <akashsonu7744@gmail.com>
2026-02-08 11:21:44 +0530

    indexOf&CompareTo

---
+++ StringsProgram/src/basicOfStrings/indexOf05.java
@@ -0,0 +1,16 @@
+package basicOfStrings;
+
+public class indexOf05 {
+
+	public static void main(String[] args) {
+		String s = "Akash Sonu";
+		System.out.println(s.indexOf('m'));
+		System.out.println(s.indexOf('A'));
+		System.out.println(s.lastIndexOf('a'));
+		
+		String a = "bbcqqqq";
+		String b = "abc";
+		System.out.println(a.compareTo(b));
+	}
+
+}
