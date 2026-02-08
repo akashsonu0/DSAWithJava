1cc4f5bd624e4d0c2f28c389a2864bdff3fd2355
akashsonu0 <akashsonu7744@gmail.com>
2026-02-08 10:51:56 +0530

    builtInStringMethod

---
+++ StringsProgram/src/basicOfStrings/builtinStringMethod.java
@@ -0,0 +1,13 @@
+package basicOfStrings;
+
+public class builtinStringMethod {
+
+	public static void main(String[] args) {
+		String s = "Akash Sonu";
+		System.out.println(s.charAt(2));
+		//System.out.println(s.charAt(14)); // Error
+		int n = s.length();
+		System.out.println(n);
+	}
+
+}
