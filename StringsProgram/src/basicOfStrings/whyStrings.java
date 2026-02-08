570ec51fed97f71982ab1585c10c747a8ad4751a
akashsonu0 <akashsonu7744@gmail.com>
2026-02-08 10:36:52 +0530

    whyStrings

---
+++ StringsProgram/src/basicOfStrings/whyStrings.java
@@ -0,0 +1,12 @@
+package basicOfStrings;
+public class whyStrings{
+	public static void main(String[] args) {
+		char[] arr = {'A','k','a','s','h'};
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]);
+		}
+		System.out.println();
+		String x = "Akash is handsome man";
+		System.out.println(x);
+	}
+}
