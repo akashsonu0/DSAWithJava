1a48702c877f54fd4303f56bffc1dd431771ef0d
akashsonu0 <akashsonu7744@gmail.com>
2026-02-09 16:23:08 +0530

    plusOperator

---
+++ StringsProgram/src/basicOfStrings/plusOperator09.java
@@ -0,0 +1,14 @@
+package basicOfStrings;
+
+public class plusOperator09 {
+
+	public static void main(String[] args) {
+		String s = "abc";
+		String t = "pqr";
+		s += t; // s = abcpqr
+		s = 10 + s;
+		System.out.println(s);
+		System.out.println("Hi I am "+10);
+	}
+
+}
