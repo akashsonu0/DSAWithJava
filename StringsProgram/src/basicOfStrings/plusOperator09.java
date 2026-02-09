31844b40d605cb8150a6d310c0fdda5ad28b7af1
akashsonu0 <akashsonu7744@gmail.com>
2026-02-09 16:29:08 +0530

    plusOperator

--- StringsProgram/src/basicOfStrings/plusOperator09.java
+++ StringsProgram/src/basicOfStrings/plusOperator09.java
@@ -9,6 +9,10 @@ public class plusOperator09 {
 		s = 10 + s;
 		System.out.println(s);
 		System.out.println("Hi I am "+10);
+		
+		System.out.println("abc"+10+20);
+		System.out.println(10+20+"abc");
+
 	}
 
 }
