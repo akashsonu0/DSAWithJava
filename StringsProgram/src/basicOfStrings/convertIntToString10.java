edef11722b78b68de64812efb780a0261113ff56
akashsonu0 <akashsonu7744@gmail.com>
2026-02-09 16:33:35 +0530

    convertIntToString

---
+++ StringsProgram/src/basicOfStrings/convertIntToString10.java
@@ -0,0 +1,16 @@
+package basicOfStrings;
+
+import java.util.Scanner;
+
+public class convertIntToString10 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number :");
+		int n = sc.nextInt();
+		String s ="";
+		s += n;
+		System.out.println(s);
+	}
+
+}
