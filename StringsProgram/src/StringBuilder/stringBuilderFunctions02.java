a71be68aea9d2e9d277343ac1e6ec4686433faec
akashsonu0 <akashsonu7744@gmail.com>
2026-02-10 17:18:40 +0530

    StringBuilderFunctions

---
+++ StringsProgram/src/StringBuilder/stringBuilderFunctions02.java
@@ -0,0 +1,12 @@
+package StringBuilder;
+
+public class stringBuilderFunctions02 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder("akash");
+		StringBuilder tb = new StringBuilder("jkash");
+		System.out.println(sb.compareTo(tb));
+		System.out.println(sb.reverse());
+	}
+
+}
