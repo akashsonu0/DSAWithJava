842454b435a9b106f7cb44ab03a9589b9c2d7a3f
akashsonu0 <akashsonu7744@gmail.com>
2026-02-10 17:24:25 +0530

    inputStringBuilder

---
+++ StringsProgram/src/StringBuilder/inputStringBuilder03.java
@@ -0,0 +1,13 @@
+package StringBuilder;
+
+import java.util.Scanner;
+
+public class inputStringBuilder03 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder(sc.nextLine());
+		System.out.println(sb);
+	}
+
+}
