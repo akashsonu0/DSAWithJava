a72f87b994cc352a037379a5a238a8968b4c1ebc
akashsonu0 <akashsonu7744@gmail.com>
2026-02-11 16:29:02 +0530

    deleteAndInsert

--- StringsProgram/src/StringBuilder/deleteAndInsert06.java
+++ StringsProgram/src/StringBuilder/deleteAndInsert06.java
@@ -7,8 +7,8 @@ public class deleteAndInsert06 {
 		System.out.println(sb);
 		sb.deleteCharAt(3);
 		System.out.println(sb); // abcef
-		sb.append("xyz");
-		System.out.println(sb); // abcefxyz , cefx(2,5)
+//		sb.append("xyz");
+//		System.out.println(sb); // abcefxyz , cefx(2,5)
 		sb.delete(2, 6); // deletes characters from 2 to 5
 		System.out.println(sb); // abyz
 		sb.insert(2,'g');
