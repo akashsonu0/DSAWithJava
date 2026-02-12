0f84d209209cbef40fb722d9ae923fc0632d0ff7
akashsonu0 <akashsonu7744@gmail.com>
2026-02-12 10:57:25 +0530

    passingStringToFunctions

---
+++ StringsProgram/src/StringBuilder/maxValue11.java
@@ -0,0 +1,20 @@
+package StringBuilder;
+
+public class maxValue11 {
+	public static void main(String[] args) {
+		String[] arr = {"455","-746745","56354","0025"};
+		// 1st Method
+		/*int mx = Integer.MIN_VALUE;
+		for(int i=0;i<arr.length;i++) {
+			int n = Integer.parseInt(arr[i]);
+			mx = Math.max(mx, n);
+		}
+		System.out.println(mx);*/
+		
+		// 2nd Method
+		
+		
+		
+	}
+
+}
---
+++ StringsProgram/src/StringBuilder/passingStringToFunctions10.java
@@ -0,0 +1,14 @@
+package StringBuilder;
+
+public class passingStringToFunctions10 {
+	public static void main(String[] args) {
+		String s = "raghav";
+		System.out.println(s);
+		change(s);
+		System.out.println(s);
+	}
+	public static void change(String s) {
+		s= "madhav";
+	}
+
+}
