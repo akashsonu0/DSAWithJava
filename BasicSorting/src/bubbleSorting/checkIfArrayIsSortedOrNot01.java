308be3f4735baddd4a4bacfa27c42a2098066344
akashsonu0 <akashsonu7744@gmail.com>
2026-02-21 16:31:49 +0530

    checkIfArrayIsSortedOrNot

---
+++ BasicSorting/src/bubbleSorting/checkIfArrayIsSortedOrNot01.java
@@ -0,0 +1,19 @@
+package bubbleSorting;
+
+public class checkIfArrayIsSortedOrNot01 {
+
+	public static void main(String[] args) {
+		int[] arr = {1,7,3,8,9,12,13};
+		int n = arr.length;
+		boolean flag = true;
+		for(int i=0;i<n-1;i++) {
+			if(arr[i]>arr[i+1]) {
+				flag = false;
+				break;
+			}
+		}
+		if(flag==true) System.out.println("Sorted");
+		else System.out.println("Unsorted");
+	}
+
+}
