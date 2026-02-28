d1de98ea42611e1723d4f91ca1ef8d2febd1c7ea
akashsonu0 <akashsonu7744@gmail.com>
2026-02-28 09:31:55 +0530

    insertionSorting

---
+++ BasicSorting/src/sorting/insertionSorting04.java
@@ -0,0 +1,36 @@
+package sorting;
+
+public class insertionSorting04 {
+	public static void print(int[] arr) {
+		for(int ele : arr) {
+			System.out.print(ele+" ");
+		}
+		 System.out.println();
+	}
+	public static void main(String[] args) {
+		int[] arr = {10,-4,20,7,-6,8};
+		int n = arr.length;
+		print(arr);
+		// Insertion Sort
+	   /*for(int i=1;i<n;i++) { // n-1 passes
+			for(int j=i;j>=1;j--) {
+				if(arr[j]<arr[j-1]) {
+					int temp = arr[j];
+					arr[j] = arr[j-1];
+					arr[j-1] = temp;
+				} else break;
+		   }		
+	   }*/	
+		// Insertion Sort
+		for(int i=1;i<n;i++) { // n-1 passes
+			int j = i;
+			while(j>=1 && arr[j]<arr[j-1]) {
+				int temp = arr[j];
+				arr[j] = arr[j-1];
+				arr[j-1] = temp;
+				j--;
+			}		
+		}
+		print(arr);
+	}
+}
