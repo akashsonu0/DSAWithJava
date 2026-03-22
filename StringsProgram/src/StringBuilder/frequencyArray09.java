df986f969fb7d1522b8d7a097c8fc0789cf64a51
akashsonu0 <akashsonu7744@gmail.com>
2026-03-22 11:39:40 +0530

    frequencyArray

--- StringsProgram/src/StringBuilder/frequencyArray09.java
+++ StringsProgram/src/StringBuilder/frequencyArray09.java
@@ -5,22 +5,24 @@ import java.util.Scanner;
 public class frequencyArray09 {
 
 	public static void main(String[] args) {
-		String s = "abaaacrmnrrrrrra";
-		int n = s.length();
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
 		int[] freq = new int[26];
-		for(int i=0;i<n;i++) {
+		// 0-a, 1-b, 2-c, ....25-z
+		for(int i=0;i<s.length();i++) {
 			char ch = s.charAt(i);
-			int idx = (int)ch-97;
+			int idx = (int)ch - 97; // storing the index
 			freq[idx]++;
+			//freq[(int)s.charAt(i) - 97]++;	
 		}
 		int maxFreq = -1;
-		for(int i=0;i<n;i++) {
+		for(int i=0;i<freq.length;i++) {
 			maxFreq = Math.max(maxFreq, freq[i]);
 		}
-		for(int i=0;i<n;i++) {
+		for(int i=0;i<freq.length;i++) {
 			if(freq[i]==maxFreq) {
-				char ch = (char) (i+97);
-				System.out.println(ch);
+				char ch = (char)(i+97);
+				System.out.print(ch+" ");
 			}
 		}
 		
