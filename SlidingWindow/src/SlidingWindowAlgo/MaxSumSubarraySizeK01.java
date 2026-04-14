7bdbc86bc9a32f54292d5a539c8b77e3b352cc70
akashsonu0 <akashsonu7744@gmail.com>
2026-04-14 16:11:11 +0530

    MaxSumSubarraySizeK

---
+++ SlidingWindow/src/SlidingWindowAlgo/MaxSumSubarraySizeK01.java
@@ -0,0 +1,31 @@
+package SlidingWindowAlgo;
+public class MaxSumSubarraySizeK01 {
+	public static void main(String[] args) {
+		int[] arr = {10,20,1,3,-40,80,10};
+		int k = 3;
+		int n = arr.length;
+		int maxSum = 0;
+		// Brute force method
+		/*for(int i=0;i<n-k+1;i++) {
+			int sum = 0;
+			for(int j=i;j<=i+k-1;j++) {
+				sum += arr[j];
+			}
+			maxSum = Math.max(maxSum, sum);
+		}
+		System.out.println(maxSum);*/
+		
+		// Optimizd sliding window method
+		int i=0 , j=k-1 , sum=0;
+		for(int a=0;a<=k-1;a++) {
+			sum += arr[a];
+		}
+		i++; j++;
+		while(j<n) {
+			sum = sum - arr[i-1] + arr[j];
+			maxSum = Math.max(maxSum, sum);
+			i++; j++;
+		}
+		System.out.println(maxSum);
+	}
+}
