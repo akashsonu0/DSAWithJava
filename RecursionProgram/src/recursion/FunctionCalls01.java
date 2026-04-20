c1c7e2071176ecd18116d19faf21660cf7833929
akashsonu0 <akashsonu7744@gmail.com>
2026-04-20 15:09:00 +0530

    functionCall

---
+++ RecursionProgram/src/recursion/FunctionCalls01.java
@@ -0,0 +1,21 @@
+package recursion;
+public class FunctionCalls01 {
+	public static void mango() {
+		System.out.println("Hi, I am in mango");		
+	}
+	public static void banana() {
+		mango();
+		System.out.println("Hi, I am in banana");	
+	}
+	public static void apple() {
+		banana();
+		System.out.println("Hi, I am in apple");
+		mango();
+	}
+	public static void main(String[] args) {
+		System.out.println("Hi, I am in mein method");
+		apple();
+
+	}
+
+}
