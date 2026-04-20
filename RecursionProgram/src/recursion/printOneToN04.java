83182d1bed81ffb6b64def34261b614f1458214e
akashsonu0 <akashsonu7744@gmail.com>
2026-04-20 16:13:48 +0530

    printOneToN

--- RecursionProgram/src/recursion/printOneToN04.java
+++ RecursionProgram/src/recursion/printOneToN04.java
@@ -1,10 +1,31 @@
 package recursion;
-
+import java.util.Scanner;
 public class printOneToN04 {
-
+	
+	// 1st method (Global Variable)
+	/*static int n; // global variable
+	public static void print(int x) {
+		if(x>n) return; // base case
+		System.out.print(x+" "); // work
+		print(x+1); // call
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n : ");
+	    n = sc.nextInt(); // local variable
+		print(1); */
+	
+	// 2nd and best method(Extra Parameter)
+	public static void print(int x,int n) {
+		if(x>n) return; // base case
+		System.out.print(x+" "); // work
+		print(x+1,n); // call
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n : ");
+	    int n = sc.nextInt(); // local variable
+		print(1,n);
 	}
 
 }
