a94658ad00db4542428cf104067ad28dbbe18c2e
akashsonu0 <akashsonu7744@gmail.com>
2026-04-20 15:59:56 +0530

    printNToOne

---
+++ RecursionProgram/src/recursion/printNToOne03.java
@@ -0,0 +1,16 @@
+package recursion;
+import java.util.Scanner;
+public class printNToOne03 {
+	public static void print(int n) {
+		if(n==0) return;
+		System.out.print(n+" ");
+		print(n-1);
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n : ");
+		int n = sc.nextInt();
+		print(n);
+	}
+
+}
