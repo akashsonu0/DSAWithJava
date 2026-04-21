989d0a6820ecd2bcffe779acacf0535035ecf146
akashsonu0 <akashsonu7744@gmail.com>
2026-04-21 16:10:13 +0530

    factorial

--- RecursionProgram/src/recursion/Factorial02.java
+++ RecursionProgram/src/recursion/Factorial02.java
@@ -2,7 +2,7 @@ package recursion;
 import java.util.Scanner;
 public class Factorial02 {
 	public static int fact(int n) {
-		if(n==1) return 1;  // base case
+		if(n==1 || n==0) return 1;  // base case
 		int ans = n * fact(n-1); // work
 		return ans; // call
 	}
