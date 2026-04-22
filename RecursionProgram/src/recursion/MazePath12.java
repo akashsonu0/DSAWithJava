e59663b751568f5b0ed4d4ee8e661c59cc6572fb
akashsonu0 <akashsonu7744@gmail.com>
2026-04-22 17:37:48 +0530

    MaizePath

---
+++ RecursionProgram/src/recursion/MazePath12.java
@@ -0,0 +1,30 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class MazePath12 {
+	// 1st method
+	public static int maze(int row, int col , int m , int n) {
+		if(row==m || col==n) return 1;
+		int rightWays = maze(row,col+1,m,n);
+		int downWays = maze(row+1,col,m,n);
+		return rightWays + downWays;
+	}
+	// 2nd method
+	public static int maze2(int m , int n) {
+		if(m==1 || n==1) return 1;
+		int rightWays = maze2(m,n-1);
+		int downWays = maze2(m-1,n);
+		return rightWays + downWays;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter n: ");
+		int n = sc.nextInt();
+		System.out.print("Enter m: ");
+		int m = sc.nextInt();
+		//System.out.println(maze(1,1,m,n));
+		System.out.println(maze2(m,n));
+	}
+
+}
