59079fb98a7899159c99fbf43a9df0b19daea1c4
akashsonu0 <akashsonu7744@gmail.com>
2026-04-23 17:25:11 +0530

    preInPost

--- RecursionProgram/src/recursion/preInPost13.java
+++ RecursionProgram/src/recursion/preInPost13.java
@@ -2,11 +2,11 @@ package recursion;
 public class preInPost13 {
 	public static void pip(int n) {
 		if(n==0) return;
-		System.out.println(n);	
+		System.out.println(n); // pre
 		pip(n-1);
-		System.out.println(n);	
+		System.out.println(n); // in
 		pip(n-1);
-		System.out.println(n);	
+		System.out.println(n); //post
 	}
 		
 	public static void main(String[] args) {
