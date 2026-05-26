845a564f6253479491c66f1a1a0e03be3487fc6a
akashsonu0 <akashsonu7744@gmail.com>
2026-05-26 10:40:30 +0530

    ShallowCopyOfANode

---
+++ LinkedList/src/Linked_List/ShallowCopyOfANode02.java
@@ -0,0 +1,15 @@
+package Linked_List;
+
+public class ShallowCopyOfANode02 {
+	public static void main(String[] args) {
+		Node a = new Node(100);
+		System.out.println(a.val);
+		Node temp = a; // shalow Copy
+		// Node temp = new Node(100); // deep copy
+		temp.val = 12;
+		System.out.println(a.val);
+		
+
+	}
+
+}
