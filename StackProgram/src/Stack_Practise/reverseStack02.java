72635683829b0d0d055db363817e31622de3119c
akashsonu0 <akashsonu7744@gmail.com>
2026-09-15 10:59:24 +0530

    reverseStack

---
+++ StackProgram/src/Stack_Practise/reverseStack02.java
@@ -0,0 +1,25 @@
+package Stack_Practise;
+
+import java.util.Stack;
+
+public class reverseStack02 {
+	public static void main(String[] args) {
+		Stack<Integer> st_original = new Stack<>();
+		st_original.push(1);
+		st_original.push(5);
+		st_original.push(10);
+		st_original.push(20);
+		System.out.println(st_original);
+		
+		Stack<Integer> st_reversed = new Stack<>();
+		while(!st_original.isEmpty()) {
+//			int element = st_original.pop();
+//			st_reversed.push(element);
+			st_reversed.push(st_original.pop());
+			
+		}
+		System.out.println(st_reversed);
+		
+	}
+
+}
