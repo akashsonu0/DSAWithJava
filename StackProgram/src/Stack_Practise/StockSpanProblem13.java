a6d6afc4974ed3ad9fac35d3b4b7d043ee66c5af
akashsonu0 <akashsonu7744@gmail.com>
2026-09-22 18:20:12 +0530

    StockSpanProblem

---
+++ StackProgram/src/Stack_Practise/StockSpanProblem13.java
@@ -0,0 +1,47 @@
+package Stack_Practise;
+import java.util.Arrays;
+import java.util.Stack;
+// Brute force
+/*
+public class StockSpanProblem13 {
+	static void calculateSpan(int[] price, int n, int[] S) {
+		S[0] = 1;
+		for(int i=1;i<n;i++) {
+			S[i] = 1;
+			for(int j=i-1;j>=0 && (price[i] >= price[j]);j--)
+				S[i]++;
+		}
+	}
+	static void printArray(int[] arr) {
+		System.out.print(Arrays.toString(arr));
+	}
+	public static void main(String[] args) {
+		int price[] = {10 , 4 , 5 , 90 , 120 , 80};
+		int n = price.length;
+		int S[] = new int[n];
+		calculateSpan(price,n,S);
+		printArray(S);
+		*/
+
+// Optimized approach
+public class StockSpanProblem13 {
+    public static void main(String[] args) {
+        int price[] = {10, 4, 5, 90, 120, 80};
+        int n = price.length;
+        int[] arr = new int[n];
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
+        for (int i = 1; i < n; i++) {
+            while (!st.isEmpty() && price[i] >= price[st.peek()]) {
+                st.pop();
+            }
+            if (st.isEmpty())
+                arr[i] = i + 1;
+            else
+                arr[i] = i - st.peek();
+            st.push(i);
+        }
+        arr[0] = 1;
+        System.out.println(Arrays.toString(arr));
+    }
+}
